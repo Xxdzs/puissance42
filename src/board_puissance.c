5656f117786441e902b96dd7084de79ea6c2544e
Julia ATES <jates-@e1r3p6.42.fr>
2018-12-22 16:19:43 +0100

    board printing function written but not tested yet

--- src/board_puissance.c
+++ src/board_puissance.c
@@ -6,7 +6,7 @@
 /*   By: jates- <jates-@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/22 12:24:50 by jates-            #+#    #+#             */
-/*   Updated: 2018/12/22 15:06:56 by jates-           ###   ########.fr       */
+/*   Updated: 2018/12/22 16:18:21 by jates-           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,54 @@
 #include "puissance.h"
 #include <stdlib.h>
 
+/*
+** Fonction which initialize the main structure of puissance 4
+*/
+
 void	new_game_state(t_game_state *game)
 {
 	game->board = *fta_alloc(game->width * game->height);
 	game->start_player = (rand() * 2) % 2 + HUMAN;
 
+}
+
+/*
+** Fonction which prints the board
+*/
+
+void	print_board(t_game_state *game)
+{
+	int i;
+	int j;
+	int	cell;
+
+	i = 0;
+	j = 0;
+	
+	write(1, " ", 1);
+	while (j++ < game->width)
+		ft_printf("%d", j % 10);
+	j = 0;
+	while(i < game->height)
+	{
+		ft_printf("%d", (game->height - i) % 10);
+		while(j < game->width)
+		{
+			j++;
+			cell = ARRAY_GETT(uint8_t, &(game->board), game->width * i + j);
+			if (cell == EMPTY)
+				write(1, " ", 1);
+			else if (cell == HUMAN)
+				write(1, "*", 1);
+			else if (cell == BOT)
+				write(1, "O", 1);
+		}
+		ft_printf("%d", (game->height - i) % 10);
+		i++
+		j = 0;
+	}
+	write(1, " ", 1);
+	while (j++ < game->width)
+		ft_printf("%d", j % 10);
 }
 
