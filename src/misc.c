26ae514ced0f51f6959cf8c0ccd7e6bf54a61046
Antoine <sid.xxdzs@gmail.com>
2018-12-23 22:05:27 +0100

    Add utilities

---
+++ src/misc.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   misc.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sid <angagnie@student.42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/23 21:56:21 by sid               #+#    #+#             */
+/*   Updated: 2018/12/23 22:03:41 by sid              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "puissance.h"
+#include "ft_array.h"
+
+unsigned			column_top(const t_game_state *game, unsigned column)
+{
+	unsigned	lign;
+
+	lign = game->height;
+	while (--lign > 0 && ARRAY_GETL(uint8_t,
+			(const t_array*)game, game->width * lign + column) == EMPTY)
+		;
+	if (ARRAY_GETL(uint8_t, (const t_array*)game, column) == EMPTY)
+		return (0);
+	return (game->height);
+}
+
+t_array				create_layer(const t_game_state *game)
+{
+	t_array		layer;
+	unsigned	col;
+
+	layer = NEW_ARRAY(unsigned);
+	fta_reserve(&layer, game->width);
+	layer.size = game->width;
+	col = game->width;
+	while (col-- > 0)
+		ARRAY_GETL(unsigned, &layer, col) = column_top(game, col);
+	return (layer);
+}
